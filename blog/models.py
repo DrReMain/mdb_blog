d25664d0fa9b3a1458e2ba1137e5ec3b9f120447
remain <remain@divergers.com>
2017-01-01 22:07:44 +0800

    add category models

--- blog/models.py
+++ blog/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+
+class CategoryParent(models.Model):
+	icon = models.CharField(max_length=40, null=True)
+	name = models.CharField(max_length=40, null=True)
+
+
+class CategoryChild(models.Model):
+	name = models.CharField(max_length=40, null=True)
+	category_parent = models.ForeignKey(CategoryParent)
